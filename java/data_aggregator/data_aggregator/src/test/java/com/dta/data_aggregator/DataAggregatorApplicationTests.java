b8a9035c76130043392ba2cbab29efa9886c4618
Patrick <pbobyn@gmail.com>
2025-12-09 14:18:19 -0500

    Adding 2nd microservice skeleton

---
+++ java/data_aggregator/data_aggregator/src/test/java/com/dta/data_aggregator/DataAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dta.data_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
